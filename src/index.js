98838eb500ea0b2bf33fecb6980437815ee11936
Andrey <2593147@gmail.com>
2022-01-25 12:09:38 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  const last = (arr) => arr[arr.length - 1];
+
+  const isCloseBracket = (bracket) => !!bracketsConfig
+    .find(([openBracket, closeBracket]) => bracket === closeBracket);
+
+  const closeToOpenBracketsMap = bracketsConfig.reduce((acc, current) => {
+    const [openBracket, closeBracket] = current;
+    acc[closeBracket] = openBracket;
+    return acc;
+  }, {});
+
+  const resultStack = [];
+  for (const ch of str) {
+    const isOpenAndCloseSame = closeToOpenBracketsMap[ch] === ch;
+    if (isOpenAndCloseSame) {
+
+      if (last(resultStack) === ch) {
+        resultStack.pop();
+      } else {
+        resultStack.push(ch);
+      }
+
+    } else if (isCloseBracket(ch)) {
+      const closeBracket = resultStack.pop();
+      if (closeToOpenBracketsMap[ch] !== closeBracket) {
+        return false;
+      }
+
+    } else {
+      resultStack.push(ch);
+    }
+  }
+
+  return resultStack.length === 0;
 }
